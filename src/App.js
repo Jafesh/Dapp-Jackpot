1d12940646fd7bc1349321097a4e8e2af152b99d
Jafesh <m18266385535@gmail.com>
2024-04-06 20:55:13 -0700

    A

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import LandingPage from './LandingPage';
+import AboutPage from './AboutPage';
+import AccountPage from './AccountPage';
+import GamePage from './GamePage';
+// Import other components if necessary
 
 function App() {
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [userAddress, setUserAddress] = useState('');
+
+  const onWalletConnect = async () => {
+    if (window.ethereum) {
+      try {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        if (accounts.length > 0) {
+          setUserAddress(accounts[0]);
+          setWalletConnected(true);
+        }
+      } catch (error) {
+        console.error("Could not get accounts", error);
+      }
+    } else {
+      console.log('MetaMask is not installed!');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={!walletConnected ? <LandingPage onWalletConnect={onWalletConnect} /> : <Navigate to="/about" />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/account" element={<AccountPage userAddress={userAddress} />} />
+          <Route path="/game" element={<GamePage />} />
+          {/* Define other routes here */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
